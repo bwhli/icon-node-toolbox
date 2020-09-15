9f3cc26b54edea6b632d7fe890b76860e0ae84df
Brian Li <brian@brianli.com>
2020-09-15 16:42:33 +0900

    Update resize-disk.sh

--- citizen/scripts/resize-disk.sh
+++ citizen/scripts/resize-disk.sh
@@ -7,8 +7,8 @@ echo $LINODE_ID
 echo $LINODE_INSTANCE_REGION
 
 cd /home/icon/citizen && docker-compose down
-umount /dev/disk/by-id/scsi-0Linode_Volume_$LINODE_INSTANCE_REGION-$LINODE_ID-data
-e2fsck -f /dev/disk/by-id/scsi-0Linode_Volume_$LINODE_INSTANCE_REGION-$LINODE_ID-data
-resize2fs /dev/disk/by-id/scsi-0Linode_Volume_$LINODE_INSTANCE_REGION-$LINODE_ID-data
-mount /dev/disk/by-id/scsi-0Linode_Volume_$LINODE_INSTANCE_REGION-$LINODE_ID-data /mnt/$LINODE_INSTANCE_REGION-$LINODE_ID-data
+umount /dev/disk/by-id/scsi-0Linode_Volume_ctz-$LINODE_INSTANCE_REGION-$LINODE_ID-data
+e2fsck -f /dev/disk/by-id/scsi-0Linode_Volume_ctz-$LINODE_INSTANCE_REGION-$LINODE_ID-data
+resize2fs /dev/disk/by-id/scsi-0Linode_Volume_ctz-$LINODE_INSTANCE_REGION-$LINODE_ID-data
+mount /dev/disk/by-id/scsi-0Linode_Volume_ctz-$LINODE_INSTANCE_REGION-$LINODE_ID-data /mnt/ctz-block-db
 cd /home/icon/citizen && docker-compose up -d
