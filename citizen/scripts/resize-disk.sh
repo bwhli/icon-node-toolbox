f956c3b66601c458c944baa052d304465c1d4b1f
Brian Li <brian@brianli.com>
2020-09-15 16:23:17 +0900

    Update resize-disk.sh

--- citizen/scripts/resize-disk.sh
+++ citizen/scripts/resize-disk.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+LINODE_ID=$(echo /home/icon/.linode_vars/LINODE_ID)
+LINODE_INSTANCE_REGION=$(echo /home/icon/.linode_vars/LINODE_INSTANCE_REGION)
+
+echo $LINODE_ID
+echo $LINODE_INSTANCE_REGION
+
 cd /home/icon/citizen && docker-compose down
 umount /dev/disk/by-id/scsi-0Linode_Volume_$LINODE_INSTANCE_REGION-$LINODE_ID-data
 e2fsck -f /dev/disk/by-id/scsi-0Linode_Volume_$LINODE_INSTANCE_REGION-$LINODE_ID-data
