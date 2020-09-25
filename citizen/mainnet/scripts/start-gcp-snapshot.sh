37fcfb2dd2b3855bb8f8ff3165f7f37a980789ae
Brian Li <brian@brianli.com>
2020-09-25 21:10:40 +0900

    Update script.

--- citizen/mainnet/scripts/start-gcp-snapshot.sh
+++ citizen/mainnet/scripts/start-gcp-snapshot.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# Set GCP variables.
 INSTANCE_NAME=$(cat /home/icon/.gcp_vars/INSTANCE_NAME)
 ZONE_ID=$(cat /home/icon/.gcp_vars/ZONE_ID)
 REGION_ID=$(cat /home/icon/.gcp_vars/REGION_ID)
 
+# Only keep 4 newest snapshots, delete the rest.
+CURRENT_SNAPSHOTS=$(gcloud compute snapshots list --format="json(name)" --sort-by=~creationTimestamp)
+CURRENT_SNAPSHOTS_LENGTH=$(echo $CURRENT_SNAPSHOTS | jq 'length')
+
+for i in $( seq 0 $(( $CURRENT_SNAPSHOTS_LENGTH - 1 )) ); do
+  if [ $i -gt 3 ]; then
+    SNAPSHOT_NAME=$(echo $CURRENT_SNAPSHOTS | jq -r --arg index "$i" '.[$index|tonumber].name')
+    echo "Deleting $SNAPSHOT_NAME..."
+    gcloud compute snapshots delete $SNAPSHOT_NAME --quiet
+  fi
+done
+
 # Get latest block height.
 LATEST_BLOCK_HEIGHT=$(curl -s http://127.0.0.1:9000/api/v1/avail/peer | jq '.block_height')
-echo $LATEST_BLOCK_HEIGHT
 
 # Stop node for snapshot.
 sudo -u icon docker-compose -f /home/icon/citizen/docker-compose.yml down
@@ -15,7 +27,7 @@ sudo -u icon docker-compose -f /home/icon/citizen/docker-compose.yml down
 umount /dev/sdb
 
 # Take snapshot.
-gcloud compute disks snapshot $INSTANCE_NAME-citizen --snapshot-names=ctz-db-backup-bh$LATEST_BLOCK_HEIGHT --zone=$ZONE_ID --storage-location=us
+/snap/bin/gcloud compute disks snapshot $INSTANCE_NAME-citizen --snapshot-names=ctz-db-backup-bh$LATEST_BLOCK_HEIGHT --zone=$ZONE_ID --storage-location=us  --quiet
 
 # Mount disk.
 mount -o discard,defaults /dev/sdb /home/icon/citizen/data
